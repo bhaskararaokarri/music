511bded6ee50b0bd1874e2af725f0fd0cc5e3933
unknown <bhaskarnaidukarri4@gmail.com>
2020-02-26 19:31:51 -0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class UploadModel(models.Model):
+    moviename=models.CharField(max_length=30)
+    artist=models.CharField(max_length=30)
+    song=models.FileField(upload_to='songs/')
---
+++ app/views.py
@@ -0,0 +1,86 @@
+from django.contrib import messages
+from django.db.models import Q
+from .models import UploadModel
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.views.generic.base import View
+
+from app.forms import UploadForm
+
+
+
+class MusicHome(View):
+    def get(self,request):
+        return render(request,"MusicHome.html")
+
+
+class UploadSongs(View):
+    def get(self,request):
+        return render(request,"upload.html",{"data":UploadForm()})
+    def post(self,request):
+        form = UploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "successfully uploaded")
+            return redirect("upload")
+        else:
+            messages.error(request, "only mp3 files can access")
+            return render(request, "upload.html",{"data":form })
+
+
+class Viewsongs(View):
+    def get(self,request):
+        all=UploadModel.objects.all()
+        return render(request,"viewallsongs.html",{"alls":all})
+
+
+
+class DeleteSongs(View):
+    def get(self,request):
+        all = UploadModel.objects.all()
+        return render(request, "deletesongs.html", {"alls": all})
+    def post(self,request):
+        d=request.POST.getlist("t1")
+        for x in d:
+            r=UploadModel.objects.filter(song=x)
+            r.delete()
+        all = UploadModel.objects.all()
+        return render(request, "deletesongs.html", {"alls": all})
+
+
+class SearchSong(View):
+    def post(self,request):
+        sr=request.POST.get("search")
+        song=UploadModel.objects.filter(Q(moviename=sr)|Q(song=sr)|Q(artist=sr))
+        # sng=UploadModel.objects.filter(moviename=sr)or UploadModel.objects.filter(song=sr)
+        return render(request,"searchsongs.html",{"songs":song})
+
+
+class PlaySong(View):
+    def get(self,request):
+        y=request.GET.get("n")
+        return render(request,"playsong.html",{"song":y})
+
+
+
+class SearchSongs(View):
+    def get(self,request):
+        return render(request,"opensearchpage.html")
+
+
+class ArtistStong(View):
+    def get(self,request):
+        a=request.GET.get("A")
+        aa=UploadModel.objects.filter(artist=a)
+        return render(request,"artistasongs.html",{"artsong":aa})
+
+
+class MovieName(View):
+    def get(self, request):
+        movie = request.GET.get("m")
+        mn = UploadModel.objects.filter(moviename=movie)
+        return render(request,"mpvienamesongs.html",{"mnsong":mn})
+
+    pass
